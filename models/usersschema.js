c57f25c948d7424dcfd8c7585539799103f0400b
Ankit Mahale <amahale2@binghamton.edu>
2016-07-15 21:43:22 -0700

    changed mongodb link

--- models/usersschema.js
+++ models/usersschema.js
@@ -1,5 +1,5 @@
 var mongoose    =   require("mongoose");
-mongoose.connect('mongodb://threadsapp:threads123@ds049935.mlab.com:49935/threads');
+mongoose.connect('mongodb://ankit:ankit123@ds023105.mlab.com:23105/heroku_r9jpvqf0/threads');
 var mongoSchema =   mongoose.Schema;
 var userSchema  = {
     "usr_img" : String,
