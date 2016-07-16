c15aad49ffafffcbeeec1f67c1d5fe547e4e9efd
Ankit Mahale <amahale2@binghamton.edu>
2016-07-16 11:36:12 -0700

    rectified mistake

--- Serve.js
+++ Serve.js
@@ -242,5 +242,7 @@ router.route("/login")
 app.use('/',router);
 
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Example app listening on port 3000!');
+});
 console.log("Listening to PORT 3000");
