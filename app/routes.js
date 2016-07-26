71f85556ec2baf74c64c7c5f4d560764118fba5e
Ankit Mahale <amahale2@binghamton.edu>
2016-07-26 10:55:04 -0700

    Changed status code-again

--- app/routes.js
+++ app/routes.js
@@ -14,7 +14,7 @@ module.exports = function(app, passport) {
 	// show the login form
 	app.get('/login', function(req, res) {
 
-		res.status(200).json({message:'Failed to login', success: false});
+		res.status(200).json({message:'Failed to login', "status": false});
 	});
 
 	// process the login form
